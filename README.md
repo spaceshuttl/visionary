6c17f4d8dcee0842741227994406d372d476d933
libeclipse <libeclipse@gmail.com>
2016-02-22 20:57:55 +0000

    Password verification

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # Visionary Password Manager
-A smarter password manager.
 
-Conventional password managers have many flaws. They work by first generating strong, random, passwords; encrypting them with a master password; and then storing the encrypted passwords somewhere for later retrieval. Sometimes, the encrypted data can be synced other devices for ease of access.
+Conventional password managers have many flaws. They work by generating passwords, encrypting them with a master password, and then storing or syncing the encrypted passwords somewhere.
 
 There are a few problems with this approach:
 
-1. The encrypted data can be lost thereby locking the user out of all of their accounts.
+1. The encrypted data can be lost, thereby locking the user out of all of their accounts.
 2. The encrypted data can be stolen. If the user was using a weak master password, all of their accounts can be compromised.
 3. The data can only be synced to a limited number of devices.
 
 **Visionary Password Manager** improves on these shortcomings considerably:
 
-1. Your passwords are on-the-fly based on a pure algorithm. This means that the only thing that would make you lose your data is if you forget your master password.
+1. Your passwords are generated on-the-fly based on a pure algorithm. This means that the only thing that would make you lose your data is you forgetting your master password.
 2. Nothing is stored so there's nothing to steal.
-3. There are thousands of iterations, which makes bruteforcing infeasable.
-4. No need to sync data, as there's nothing to sync! You can use this script or our API (coming soon) from anywhere to generate your passwords.
+3. There are thousands of iterations, which makes brute-forcing infeasible.
+4. No need to sync data, as there's nothing to sync! You can use this script or our API *(coming soon)* from anywhere in the world, and from any device, to generate your passwords.
 
 ###NOT YET READY FOR USE!
 
--- visionary.py
+++ visionary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python2
 
 import hashlib
+import re
 from getpass import getpass
 
 def banner():
@@ -15,29 +16,45 @@ def banner():
 def sha256(s):
     return hashlib.sha256(s).hexdigest()
 
-def interactive():
-    master_password = getpass('Master password: ')
-    reenter_master_password = getpass('Re-enter master password: ')
-    if master_password == reenter_master_password:
-        keyword = raw_input('Keyword: ')
-        print '\nYour password: %s' % (generate(master_password, keyword))
-    else:
-        print 'Passwords don\'t match!\n'
-        interactive()
     
 def generate(master_password, keyword):
     password = sha256(sha256(master_password)+sha256(keyword))
-    for iteration in range(500000):
+    for iteration in range(100000):
         password = sha256(password)
-    return password    
+    return password  
+
+def strong(password):
+    if len(password) >= 14:
+        if re.search(r'\d', password):
+            if re.search(r'[A-Z]', password) and re.search(r'[a-z]', password):
+                return True
+            else:
+                return 'Password must be a mix of uppercase and lowercase characters.'
+        else:
+            return 'Password must contain digits.'
+    else:
+        return 'Password must be at least 14 characters long.'
 
 def main():
-    print '%s\n' % (banner())
-    interactive()    
+    master_password = getpass('Master password: ')
+    strong_enough = strong(master_password)
+    if strong_enough == True:
+        reenter_master_password = getpass('Re-enter master password: ')
+        if master_password == reenter_master_password:
+            keyword = raw_input('Keyword: ')
+            print '\nYour password: %s' % (generate(master_password, keyword))
+        else:
+            print 'Passwords don\'t match!\n'
+            main()
+            raise SystemExit
+    else:
+        print '%s\n' % (strong_enough)
+        main()
+        raise SystemExit        
  
 if __name__ == "__main__":
     try:
+        print '%s\n' % (banner())
         main()
     except KeyboardInterrupt:
         print '\nKeyboard Interrupt.'
-        raise SystemExit
