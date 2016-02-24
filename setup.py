c3e7845f3161a9f2d914835b51fca97122a99a86
libeclipse <libeclipse@gmail.com>
2016-02-24 17:49:50 +0000

    Added scrypt key generation.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 setup(name='visionarypm',
-      version='0.1',
+      version='0.2',
       description='A smarter password manager.',
       url='https://github.com/libeclipse/visionary',
       author='libeclipse',
       author_email='libeclipse@gmail.com',
       license='GPLv3',
       packages=['visionarypm'],
+      install_requires=["pyscrypt"],
       entry_points = {'console_scripts': ['visionarypm = visionarypm:main']},
+      keywords = ['password', 'manager', 'visionary', 'visionarypm'],
       zip_safe=False)
