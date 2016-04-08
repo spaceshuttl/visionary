3c00fa56daae5549cd90a690cefae97acf2d9537
libeclipse <libeclipse@gmail.com>
2016-04-08 15:49:23 +0100

    Pypi being retarded.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='visionarypm',
-      version='0.6.3',
+      version='0.6.4',
       description='A smarter password manager.',
       url='https://github.com/libeclipse/visionary',
       author='libeclipse',
