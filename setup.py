f5ddb0bb6c7fcf9733fd2cd41da6bc2adf35445b
libeclipse <libeclipse@gmail.com>
2016-04-08 15:38:34 +0100

    v#

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='visionarypm',
-      version='0.6.2',
+      version='0.6.3',
       description='A smarter password manager.',
       url='https://github.com/libeclipse/visionary',
       author='libeclipse',
