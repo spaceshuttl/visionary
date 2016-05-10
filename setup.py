d8078910f86234692fa569b6d837fcea121013fb
libeclipse <libeclipse@gmail.com>
2016-05-10 19:19:46 +0000

    Updated version number.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='visionarypm',
-      version='5.0.0',
+      version='5.0.1',
       description='A smarter password manager.',
       long_description = 'Manages your passwords without storing anything.',
       url='https://github.com/libeclipse/visionary',
